c81db7f8fec9605f25bebdc1239aa6965533a444
yn-jee <nayj2842@gmail.com>
2025-04-13 01:41:07 +0900

    Initial Commit

---
+++ SnackBar/SnackBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackBarApp.swift
+//  SnackBar
+//
+//  Created by 나윤지 on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct SnackBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnackBarTests/SnackBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnackBarTests.swift
+//  SnackBarTests
+//
+//  Created by 나윤지 on 4/13/25.
+//
+
+import Testing
+@testable import SnackBar
+
+struct SnackBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
